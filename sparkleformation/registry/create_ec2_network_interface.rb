377539b502cbe6f438a3a971393286d4971a77ee
Greg Swallow <gswallow@gmail.com>
2016-12-20 10:50:58 -0500

    Lambda were leaving dangling network interfaces around.  Fix.

--- sparkleformation/registry/create_ec2_network_interface.rb
+++ sparkleformation/registry/create_ec2_network_interface.rb
@@ -1,5 +1,5 @@
 SfnRegistry.register(:create_ec2_network_interface) do
-  { 'Action' => %w(ec2:CreateNetworkInterface),
+  { 'Action' => %w(ec2:CreateNetworkInterface ec2:DeleteNetworkInterface ec2:DescribeNetworkInterfaces),
     'Resource' => %w( * ),
     'Effect' => 'Allow'
   }
