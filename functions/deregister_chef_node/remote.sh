0deeff236b9db2773dfa05ce659a3754fcfc6f6e
Greg Swallow <gswallow@gmail.com>
2016-12-28 21:58:13 -0500

    That was easy.

---
+++ functions/deregister_chef_node/remote.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+knife_rb=${knife_rb:=~/lambda.rb}
+chef_key=${chef_key:=~/lambda.pem}
+
+sudo yum -y update
+sudo yum -y groupinstall "Development Tools"
+
+virtualenv /tmp/pychef
+source /tmp/pychef/bin/activate
+pip install pychef
+
+mkdir -p /tmp/deregister_chef_node/.chef
+cp -r /tmp/pychef/lib/python2.7/site-packages/* /tmp/deregister_chef_node
+cp -r .chef /tmp/deregister_chef_node
+cp $chef_key /tmp/deregister_chef_node/.chef/lambda.pem
+cp $knife_rb /tmp/deregister_chef_node/.chef/knife.rb
+
+pushd /tmp/deregister_chef_node
+chmod -R 755 deregister_chef_node.py .chef
+chmod 644 .chef/lambda.pem
+zip -r ../deregister_chef_node.zip * .chef
+popd
+
+cp /tmp/deregister_chef_node.zip .
+rm -rf /tmp/deregister_chef_node /tmp/deregister_chef_node.zip /tmp/pychef $knife_rb $chef_key
