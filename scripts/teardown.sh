dc3dba68ab615b437bbbd37c43041a9980e9e0b8
Greg Swallow <gswallow@gmail.com>
2017-10-11 16:22:36 -0400

    Use del_stack function.

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -14,11 +14,26 @@ run_if_yes () {
   esac
 }
 
+del_stack () {
+  stack=$1
+  run_if_yes "aws cloudformation delete-stack --stack-name $stack"
+  echo -n "Waiting for resource deletion"
+  while [ "$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[].StackStatus' --output text)" == "DELETE_IN_PROGRESS" ]; do
+    echo -n .
+    sleep 1
+  done
+  echo
+  if [ "$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[].StackStatus' --output text)" == "DELETE_FAILED" ]; then
+    echo "$stack failed to delete"
+    exit 1
+  fi
+}
+
 # Tear down the stacks
 for stack in $(aws cloudformation list-stacks \
  --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE DELETE_FAILED \
  --query 'StackSummaries[?contains(StackName, `'${org}'-'${environment}'-lambda`) == `true`].StackId' \
  --output text); do
 
-  run_if_yes "aws cloudformation delete-stack --stack-name $stack"
+  del_stack $stack
 done
