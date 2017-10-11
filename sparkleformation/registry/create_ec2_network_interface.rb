d293c61e262626e53388e9dbf7ba786e321aba4a
Greg Swallow <gswallow@gmail.com>
2017-10-11 16:22:12 -0400

    Lambdas are leaving orphaned ENIs behind.

--- sparkleformation/registry/create_ec2_network_interface.rb
+++ sparkleformation/registry/create_ec2_network_interface.rb
@@ -1,5 +1,12 @@
 SfnRegistry.register(:create_ec2_network_interface) do
-  { 'Action' => %w(ec2:CreateNetworkInterface ec2:DeleteNetworkInterface ec2:DescribeNetworkInterfaces),
+  { 'Action' => %w(ec2:AttachNetworkInterface
+                   ec2:CreateNetworkInterface
+                   ec2:DeleteNetworkInterface
+                   ec2:DescribeNetworkInterfaces
+                   ec2:DescribeNetworkInterfaceAttribute
+                   ec2:DetachNetworkInterface
+                   ec2:ModifyNetworkInterfaceAttribute
+                   ec2:ResetNetworkInterfaceAttribute),
     'Resource' => %w( * ),
     'Effect' => 'Allow'
   }
